857e3b728e97c7a06ebdf4b76d069760732b756b
net6825 <80778023+net6825@users.noreply.github.com>
2022-03-11 01:45:16 +0900

    f

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,15 +15,36 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+            //insert
 //            Member member = new Member();
-//            member.setId(2L);
-//            member.setName("HelloB");
+//            member.setId(1L);
+//            member.setName("HelloA");
 //            em.persist(member);
-            Member findMember = em.find(Member.class, 1L);
-            System.out.println("findMember = " + findMember.getId());
-            System.out.println("findMember = " + findMember.getName());
+
+            //select
+//            Member findMember = em.find(Member.class, 1L);
+//            System.out.println("findMember = " + findMember.getId());
+//            System.out.println("findMember = " + findMember.getName());
+
+            //delete
+//            Member findMember = em.find(Member.class, 1L);
+//            em.remove(findMember);
+
+            //update
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("HelloJPA");         //따로 em.persist(findMember); 해주지 않아도 update가 됨
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.getName() = " + member.getName());
+            }
 
             tx.commit();
+
         } catch (Exception e) {
             tx.rollback();
         } finally {
